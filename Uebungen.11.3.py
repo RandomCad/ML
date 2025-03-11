bb8e8289a07e806d038f1d482bec2fb75ed4a04f
RandomCad <mmont4850@gmail.com>
2025-03-11 08:53:01 +0100

    Functional approach exercises

--- Uebungen.11.3.py
+++ Uebungen.11.3.py
@@ -13,10 +13,34 @@ print (b)
 print (c)
 print (d)
 
-squars = { (i, i**2) for i in range (0,100)}
+squars = { i: i**2 for i in range (0,100)}
 print (squars)
-keys = [i[0] for i in squars]
+keys = [i for i in squars]
 print (keys)
 
 evan = [i for i in range(1,100) if i%2==0]
 print (evan)
+
+def degToFahr(deg):
+    return deg * 1.8 + 32
+
+print ("fahrenheit:")
+fahrenheit = list(map(degToFahr, range(1,101)))
+print (fahrenheit)
+
+highTemp = filter( lambda x: x > 100 , fahrenheit)
+print (sum(highTemp))
+
+def revList(list):
+    return list[::-1]
+
+print (list(revList(range(1,100))))
+
+x = [i for i in range(1,10)]
+y = [i for i in range(4,13)]
+print (x,y)
+points = [(x[i], y[i]) for i in range (0,len(x))]
+print (points)
+xPos = [points[i][0] for i in range (0, len(points))]
+yPos = [points[i][1] for i in range (0, len(points))]
+print (xPos, yPos)
